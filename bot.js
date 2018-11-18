591ee7cc45947a74ea8ff43facfc424636171434
MrKrisps <45145562+MrKrisps@users.noreply.github.com>
2018-11-18 21:42:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -259,7 +259,7 @@ if (message.content === 'Kspaam') {
 
 
 client5.on('message', message => { 
-if (message.content === 'Kspaam') {
+if (message.content === 'Kspamm') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
